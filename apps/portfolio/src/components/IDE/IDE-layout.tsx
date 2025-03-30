8357e735a183d74d01767cd0381f317ce82e8a30
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-03-30 20:50:35 +0700

    feat(portfolio): add resizable panels and home navigation for enhanced IDE layout

--- apps/portfolio/src/components/IDE/IDE-layout.tsx
+++ apps/portfolio/src/components/IDE/IDE-layout.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
+
+import { ResizablePanel, ResizablePanelGroup } from "../resizable/resizable";
 
 import { DraggableWindow } from "./draggable-window";
 import { FileSidebar } from "./file-sidebar";
@@ -9,6 +11,12 @@ import { Tabs } from "./tabs";
 import { TitleBar } from "./title-bar";
 
 export const IDELayout = ({ children }: PropsWithChildren) => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
     <div className="flex min-h-screen w-screen items-center justify-center overflow-hidden p-0 md:p-4">
       <DraggableWindow>
@@ -17,19 +25,23 @@ export const IDELayout = ({ children }: PropsWithChildren) => {
 
         {/* IDE Content */}
         <div className="h-[calc(100%-3.5rem)]">
-          <div className="flex size-full overflow-hidden">
-            <div className="hidden min-w-[150px] max-w-[300px] flex-1 overflow-hidden lg:block">
-              <FileSidebar />
-            </div>
-            <div className="flex size-full flex-col lg:flex-1">
-              <Tabs />
-              <div className="flex flex-1 overflow-y-auto bg-gray-800">
-                <main className="w-full max-w-full overflow-x-hidden px-4 pt-4">
-                  {children}
-                </main>
+          {mounted && (
+            <ResizablePanelGroup direction="horizontal">
+              <div className="hidden min-w-[150px] max-w-[300px] flex-1 overflow-hidden lg:block">
+                <FileSidebar />
               </div>
-            </div>
-          </div>
+              <ResizablePanel defaultSize={80}>
+                <div className="flex h-full flex-col">
+                  <Tabs />
+                  <div className="flex flex-1 overflow-y-auto bg-gray-800">
+                    <main className="size-full max-w-full overflow-x-hidden px-4">
+                      {children}
+                    </main>
+                  </div>
+                </div>
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          )}
         </div>
 
         {/* Footer */}
