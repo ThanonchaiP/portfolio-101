020ea3c3af7f2d01f728f9d2850671607c404aef
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-04-01 23:25:36 +0700

    fix(disclaimer): adjust text size in Disclaimer component for better readability

--- apps/election/src/components/disclaimer/disclaimer.tsx
+++ apps/election/src/components/disclaimer/disclaimer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export const Disclaimer = () => {
   return (
-    <p className="fixed bottom-1 left-1/2 hidden -translate-x-1/2 whitespace-nowrap text-base text-red-600 xl:block">
+    <p className="fixed bottom-1 left-1/2 hidden -translate-x-1/2 whitespace-nowrap text-sm text-red-600 xl:block">
       * เว็บไซต์นี้จัดทำขึ้นเพื่อการศึกษาและการสร้างผลงานส่วนตัวเท่านั้น
       ท่านสามารถเข้าไปดูเว็บไซต์ต้นฉบับได้ที่{" "}
       <Link
