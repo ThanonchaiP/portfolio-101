0d6cea19c96f6ab1b08f00d2556a85715df77323
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-11-29 16:26:38 +0700

    chore: remove .node-version and amplify configuration files

--- apps/portfolio/next.config.ts
+++ apps/portfolio/next.config.ts
@@ -3,12 +3,11 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   outputFileTracingRoot: path.join(__dirname, "../../"),
   reactStrictMode: true,
-  reactCompiler: {
-    compilationMode: "annotation",
-  },
+  // reactCompiler: {
+  //   compilationMode: "annotation",
+  // },
 };
 
 export default nextConfig;
