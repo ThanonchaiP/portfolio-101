2f69a7e97ded37cc9d7cd585c9b9beba7648f2a3
Thanonchai Paliwong <t.paliwong@gmail.com>
2025-11-29 15:56:54 +0700

    feat: add .node-version file and amplify configuration for deployment

--- apps/portfolio/next.config.ts
+++ apps/portfolio/next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   outputFileTracingRoot: path.join(__dirname, "../../"),
   reactStrictMode: true,
   reactCompiler: {
